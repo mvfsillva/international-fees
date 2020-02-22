f4091a936d94b0cf5f58c8eab6acec30b99fe054
Marcus Silva <mvfsillva@gmail.com>
2020-02-22 07:37:42 -0300

    feat: add canada fees

---
+++ src/ca.ts
@@ -0,0 +1,69 @@
+const can: object = {
+  'Alberta': {
+    provinceRate: 0,
+    countryRate: 5,
+    tax: 5
+  },
+  'British Columbia': {
+    provinceRate: 7,
+    countryRate: 5,
+    tax: 12
+  },
+  'Manitoba': {
+    provinceRate: 7,
+    countryRate: 5,
+    tax: 12
+  },
+  'New-Brunswick': {
+    provinceRate: 10,
+    countryRate: 5,
+    tax: 15
+  },
+  'Newfoundland and Labrador':{
+    provinceRate: 10,
+    countryRate: 5,
+    tax: 15
+  },
+  'Northwest Territories': {
+    provinceRate: 0,
+    countryRate: 5,
+    tax: 5
+  },
+  'Nova Scotia': {
+    provinceRate: 10,
+    countryRate: 5,
+    tax: 15
+  },
+  'Nunavut': {
+    provinceRate: 0,
+    countryRate: 5,
+    tax: 5
+  },
+  'Ontario': {
+    provinceRate: 8,
+    countryRate: 5,
+    tax: 13
+  },
+  'Prince Edward Island (PEI)': {
+    provinceRate: 10,
+    countryRate: 5,
+    tax: 15
+  },
+  'Québec': {
+    provinceRate: 9.975,
+    countryRate: 5,
+    tax: 14.975
+  },
+  'Saskatchewan': {
+    provinceRate: 6,
+    countryRate: 5,
+    tax: 11
+  },
+  'Yukon': {
+    provinceRate: 6,
+    countryRate: 5,
+    tax: 11
+  },
+}
+
+export default can
