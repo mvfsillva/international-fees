680fcf4b894d1203fb5c5c14144f72217d66bb87
Marcus Silva <mvfsillva@gmail.com>
2020-02-22 07:37:50 -0300

    feat: add usa fees

---
+++ src/usa.ts
@@ -0,0 +1,181 @@
+const usa: object = {
+'Alabama': {
+    tax: 10,
+  },
+  'Alaska': {
+    tax: 9,
+  },
+  'American Samoa': {
+    tax: 10,
+  },
+  'Arizona': {
+    tax: 8,
+  },
+  'Arkansas': {
+    tax: 7,
+  },
+  'California': {
+    tax: 3,
+  },
+  'Colorado': {
+    tax: 10,
+  },
+  'Connecticut': {
+    tax: 10,
+  },
+  'Delaware': {
+    tax: 10,
+  },
+  'District Of Columbia': {
+    tax: 10,
+  },
+  'Federated States Of Micronesia': {
+    tax: 10,
+  },
+  'Florida': {
+    tax: 7,
+  },
+  'Georgia': {
+    tax: 8,
+  },
+  'Guam': {
+    tax: 10,
+  },
+  'Hawaii': {
+    tax: 10,
+  },
+  'Idaho': {
+    tax: 10,
+  },
+  'Illinois': {
+    tax: 10,
+  },
+  'Indiana': {
+    tax: 10,
+  },
+  'Iowa': {
+    tax: 12,
+  },
+  'Kansas': {
+    tax: 10,
+  },
+  'Kentucky': {
+    tax: 15,
+  },
+  'Louisiana': {
+    tax: 10,
+  },
+  'Maine': {
+    tax: 10,
+  },
+  'Marshall Islands': {
+    tax: 11,
+  },
+  'Maryland': {
+    tax: 10,
+  },
+  'Massachusetts': {
+    tax: 9,
+  },
+  'Michigan': {
+    tax: 10,
+  },
+  'Minnesota': {
+    tax: 10,
+  },
+  'Mississippi': {
+    tax: 10,
+  },
+  'Missouri': {
+    tax: 10,
+  },
+  'Montana': {
+    tax: 10,
+  },
+  'Nebraska': {
+    tax: 10,
+  },
+  'Nevada': {
+    tax: 10,
+  },
+  'New Hampshire': {
+    tax: 10,
+  },
+  'New Jersey': {
+    tax: 1,
+  },
+  'New Mexico': {
+    tax: 10,
+  },
+  'New York': {
+    tax: 10,
+  },
+  'North Carolina': {
+    tax: 2,
+  },
+  'North Dakota': {
+    tax: 10,
+  },
+  'Northern Mariana Islands': {
+    tax: 10,
+  },
+  'Ohio': {
+    tax: 10,
+  },
+  'Oklahoma': {
+    tax: 10,
+  },
+  'Oregon': {
+    tax: 4,
+  },
+  'Palau': {
+    tax: 10,
+  },
+  'Pennsylvania': {
+    tax: 10,
+  },
+  'Puerto Rico': {
+    tax: 10,
+  },
+  'Rhode Island': {
+    tax: 10,
+  },
+  'South Carolina': {
+    tax: 10,
+  },
+  'South Dakota': {
+    tax: 10,
+  },
+  'Tennessee': {
+    tax: 10,
+  },
+  'Texas': {
+    tax: 6,
+  },
+  'Utah': {
+    tax: 10,
+  },
+  'Vermont': {
+    tax: 10,
+  },
+  'Virgin Islands': {
+    tax: 10,
+  },
+  'Virginia': {
+    tax: 10,
+  },
+  'Washington': {
+    tax: 10,
+  },
+  'West Virginia': {
+    tax: 8,
+  },
+  'Wisconsin': {
+    tax: 10,
+  },
+  'Wyoming': {
+    tax: 10,
+  },
+}
+
+export default usa
